d34005eb546eb735096e8cde2b809519bceb52d2
纵昂 <zongang15@yahoo.com>
2021-05-02 07:17:31 +0800

    first commit

--- LoginAndRegist/AppDelegate.m
+++ LoginAndRegist/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -13,28 +14,61 @@
 
 @implementation AppDelegate
 
-
+// 程序启动完成调用
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    [self AFNetworkStatus];
+    // 创建Window
+      self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+      // 设置Window的背景颜色
+      self.window.backgroundColor = [UIColor whiteColor];
+    // 设置根控制器
+    ViewController *baseView = [[ViewController alloc]init];
+    UINavigationController *nav = [[UINavigationController alloc] initWithRootViewController:baseView];
+    self.window.rootViewController = nav;
+
+      // 设置并显示主窗口
+      [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
-
-#pragma mark - UISceneSession lifecycle
-
-
-- (UISceneConfiguration *)application:(UIApplication *)application configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession options:(UISceneConnectionOptions *)options {
-    // Called when a new scene session is being created.
-    // Use this method to select a configuration to create the new scene with.
-    return [[UISceneConfiguration alloc] initWithName:@"Default Configuration" sessionRole:connectingSceneSession.role];
+- (void)AFNetworkStatus{
+ 
+    //1.创建网络监测者
+    AFNetworkReachabilityManager *manager = [AFNetworkReachabilityManager sharedManager];
+ 
+    /*枚举里面四个状态  分别对应 未知 无网络 数据 WiFi
+     typedef NS_ENUM(NSInteger, AFNetworkReachabilityStatus) {
+         AFNetworkReachabilityStatusUnknown          = -1,      未知
+         AFNetworkReachabilityStatusNotReachable     = 0,       无网络
+         AFNetworkReachabilityStatusReachableViaWWAN = 1,       蜂窝数据网络
+         AFNetworkReachabilityStatusReachableViaWiFi = 2,       WiFi
+     };
+     */
+ 
+    [manager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
+        switch (status) {
+            case AFNetworkReachabilityStatusUnknown:
+                NSLog(@"未知网络状态");
+                break;
+            case AFNetworkReachabilityStatusNotReachable:
+                NSLog(@"无网络");
+                break;
+            case AFNetworkReachabilityStatusReachableViaWWAN:
+                NSLog(@"蜂窝数据网");
+                break;
+            case AFNetworkReachabilityStatusReachableViaWiFi:
+                NSLog(@"WiFi网络");
+                break;
+            default:
+                break;
+        }
+    }] ;
 }
 
 
-- (void)application:(UIApplication *)application didDiscardSceneSessions:(NSSet<UISceneSession *> *)sceneSessions {
-    // Called when the user discards a scene session.
-    // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-    // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-}
 
 
 @end
--- LoginAndRegist/ViewController.m
+++ LoginAndRegist/ViewController.m
@@ -16,7 +16,33 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.navigationItem.title = @"登录页";
+    self.view.backgroundColor =[UIColor whiteColor];
+    
+    
+    ZATextField * account = [[ZATextField alloc]initWithFrame:CGRectMake(0, 160, SCREEN_SIZE.width, 60)];
+    account.placeholder = @"请输入手机号";
+    account.maxLength = 11;
+    account.errorStr = @"超出字数限制";
+    account.leftIconName = @"loginPhone";
+    account.textField.keyboardType = UIKeyboardTypeNumberPad;
+    [self.view addSubview:account];
+    
+    ZATextField * password = [[ZATextField alloc]initWithFrame:CGRectMake(0, 220, SCREEN_SIZE.width, 60)];
+    password.placeholder = @"请输入密码";
+    password.maxLength = 6;
+    password.errorStr = @"超出字数限制";
+    password.leftIconName = @"Key";
+    password.textField.keyboardType = UIKeyboardTypeASCIICapable;
+    password.textField.secureTextEntry = YES;
+    [self.view addSubview:password];
+    
+    
 }
 
+// 退出键盘
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [[UIApplication sharedApplication].keyWindow endEditing:YES];
+}
 
 @end
